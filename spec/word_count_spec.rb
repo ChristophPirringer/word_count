0df4f5944d7f4e43918f68249517601fb78efb1f
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-08-07 09:22:20 -0700

    Add final objective pre-integration spec. Pass all 4 specs

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -12,4 +12,12 @@ describe ("String#word_count") do
     expect(("me and myself and this and that").word_count("and")).to(eq(3))
   end
 
+  it("returns the number '2' for the word 'and' in the sentence 'me and old man anderson and his dog'") do
+    expect(("me and old man anderson and his dog").word_count("and")).to(eq(2))
+  end
+
+  it("As per exercise objective, returns the number '2' for the word 'peck' in the sentence 'If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?'") do
+    expect(("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?").word_count("peck")).to(eq(2))
+  end
+
 end
