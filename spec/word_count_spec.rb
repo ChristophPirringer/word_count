5759f77b568328cfc466d2dbabb955f59117d86e
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-08-07 09:12:28 -0700

    Add spec for multiple occurances of same word in sentence, pass the spec.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -8,4 +8,8 @@ describe ("String#word_count") do
     expect(("me and myself").word_count("and")).to(eq(1))
   end
 
+  it("returns the number '3' for the word 'and' in the sentence 'me and myself and this and that'") do
+    expect(("me and myself and this and that").word_count("and")).to(eq(3))
+  end
+
 end
